7c0472cbf9045868ba6f3a65e67e933c39ba9b04
github username <scott.gloyna01@gmail.com>
2021-02-08 22:29:55 -0600

    commit tree 1f9a5345a444f765367f2293c14af8d344775ec7

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,18 @@
 class Post < ActiveRecord::Base
+    belongs_to :user
+    has_many :post_tags
+    has_many :tags, through: :post_tags
+    
+
+    validates :name, presence: true, uniqueness: true
+    validates :content, presence: true
+
+    def post_tag_attributes=(post_tag_attributes)
+        byebug
+        post_tag_attributes.values.each do |post_tag_attribute|
+            byebug
+            post_tag = Post_tag.create(post_tag_attribute)
+        end
+    end
   
 end
